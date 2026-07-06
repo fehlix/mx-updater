4e5231217ad198bb1e54693a692941645bbc5a7f
fehlix <feh.ler@t-online.de>
2026-07-06 02:46:42 +0200

    dev04 - help l10n

---
+++ libexec/mx-updater/help_i18n_strings.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Strings used in the help documentation.
+# Listed here so xgettext picks them up with a source reference.
+# Translations go in the main l10n/po/<lang>.po files.
+_("MX Tools")
+_("System Menu")
+_("MX Updater Preferences")
